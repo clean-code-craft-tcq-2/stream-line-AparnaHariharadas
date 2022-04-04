3307c34063aac62d5125eea8d771af5dea7ab564
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-04 13:37:56 +0530

    Update and rename Sender/testStreamlineBmsSender.cpp to testStreamlineBmsSender.cpp

--- testStreamlineBmsSender.cpp
+++ testStreamlineBmsSender.cpp
@@ -1,7 +1,7 @@
 // Test driven Development. -> Define the testcase before implementation.
 #define CATCH_CONFIG_MAIN 
 
-#include "../test/catch.hpp"
+#include "test/catch.hpp"
 #include "streamlineBmsSender.h"
 
 TEST_CASE("Check whether sensor inputs are read from valid txt file") {
