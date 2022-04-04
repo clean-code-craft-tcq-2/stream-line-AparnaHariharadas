2820e45347321fc6461683afd67c0f655343d194
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-04 13:33:44 +0530

    Update testStreamlineBmsSender.cpp

--- testStreamlineBmsSender.cpp
+++ testStreamlineBmsSender.cpp
@@ -1,8 +1,8 @@
 // Test driven Development. -> Define the testcase before implementation.
 #define CATCH_CONFIG_MAIN 
 
-#include "test/catch.hpp"
-#include "Sender/streamlineBmsSender.h"
+#include "../test/catch.hpp"
+#include "streamlineBmsSender.h"
 
 TEST_CASE("Check whether sensor inputs are read from valid txt file") {
   //int CurrentRanges[] = {4,5};
