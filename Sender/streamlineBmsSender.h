1ab2afadca0ac4144e6c530ac4466609e6f01166
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-04 13:47:13 +0530

    Update streamlineBmsSender.h

--- Sender/streamlineBmsSender.h
+++ Sender/streamlineBmsSender.h
@@ -1,6 +1,10 @@
 #include<iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sstream> // for ostringstream
+#include <string.h>
+#include<array>
+#include <fstream>
 using namespace std;
 
 
