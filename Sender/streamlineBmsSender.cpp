7b3156ac3914d3f063dcacc1a4f29f5843dbade6
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-04 14:29:33 +0530

    Update streamlineBmsSender.cpp

--- Sender/streamlineBmsSender.cpp
+++ Sender/streamlineBmsSender.cpp
@@ -1,24 +1,27 @@
 #include "streamlineBmsSender.h"
-//using ifstream;
+using ifstream;
 
 
-StreamlineBMSOutput  readFromSenderTxt(int num){
+StreamlineBMSOutput  readFromSenderTxt(string inputFile){
   ifstream inDataFileHandle; 
   string line;
   senderInputParam inputParameters;
-  inDataFileHandle.open("streamlineSender.txt");
+  float Voltage, Temperature
+  inDataFileHandle.open(inputFile);
    if(!inDataFileHandle)
       return E_NOT_OK;
   while ( getline(inDataFileHandle , line)) {
     stringstream   linestream(line);
     //std::getline(linestream, data, '\t');  // read up-to the first tab (discard tab).
+    linestream >> Voltage >> Temperature;
     linestream >> inputParameters.voltage >> inputParameters.temperature;
     cout << inputParameters.voltage << inputParameters.temperature << num <<endl;
+    cout << "VOLTTEMP" <<Voltage << Temperature << num <<endl;
    }
    return E_OK;
 }
 
-int processData(int lines)
+int processData()
 {
   (void)readFromSenderTxt(lines);
   return 1;
