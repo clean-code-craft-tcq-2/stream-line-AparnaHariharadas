e1f43f43db63e33f38adb7b607624e996af1c1bb
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-04 13:07:47 +0530

    Create streamlineBmsSender.h

---
+++ Sender/streamlineBmsSender.h
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+
+typedef enum
+{
+	E_NOT_OK,
+	E_OK
+}StreamlineBMSOutput;
+
+typede struct batteryParam
+{
+  float voltage;
+  float temperature;
+}senderInputParam;
+
+StreamlineBMSOutput readFromSenderTxt(string inputFileName);
