a57100fca336ce86f36198281a0f339b03bcd963
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-04-19 22:29:45 +0530

    Update testBmsReceiver.cpp

--- testBmsReceiver.cpp
+++ testBmsReceiver.cpp
@@ -39,17 +39,7 @@ TEST_CASE("Read from the console (Sender) and print statistics for 10 samples")
     REQUIRE(BmsParam[1].MinValue == 10.5); //Min Temperature
     REQUIRE(BmsParam[1].MovingAvg == 17.5); //Avg Temperature
 
-    char expectedOutput[1000] = {"MinCurrent, MaxCurrent, AvgCurrent, MinTemperature, MaxTemperature, AvgTemperature\n\
-                                    1.5, 1.5, 0, 10.5, 10.5, 0\n\
-                                    1.5, 2.5, 0, 10.5, 11.5, 0\n\
-                                    1.5, 3.5, 0, 10.5, 12.5, 0\n\
-                                    1.5, 4.5, 0, 10.5, 13.5, 0\n\
-                                    1.5, 5.5, 3.5, 10.5, 14.5, 4.3\n\
-                                    1.5, 6.5, 5.1, 10.5, 15.5, 5.7\n\
-                                    1.5, 7.5, 6.3, 10.5, 16.5, 6.7\n\
-                                    1.5, 8.5, 7.3, 10.5, 17.5, 7.7\n\
-                                    1.5, 9.5, 8.3, 10.5, 18.5, 8.7\n\
-                                    1.5, 10.5, 9.3, 10.5, 19.5, 9.7\n"};
+    char expectedOutput[1000] = {"MinCurrent, MaxCurrent, AvgCurrent, MinTemperature, MaxTemperature, AvgTemperature\n1.50, 1.50, 0.00, 10.50, 10.50, 0.00\n1.50, 2.50, 0.00, 10.50, 11.50, 0.00\n1.50, 3.50, 0.00, 10.50, 12.50, 0.00\n1.50, 4.50, 0.00, 10.50, 13.50, 0.00\n1.50, 5.50, 3.50, 10.50, 14.50, 4.30\n1.50, 6.50, 4.50, 10.50, 15.50, 5.30\n1.50, 7.50, 5.50, 10.50, 16.50, 6.30\n1.50, 8.50, 6.50, 10.50, 17.50, 7.30\n1.50, 9.50, 7.50, 10.50, 18.50, 8.30\n1.50, 10.50, 8.50, 10.50, 19.50, 9.30\n};
     printf("%s\n",printChar);
     printf("%s\n",expectedOutput);
     REQUIRE(strcmp(printChar, expectedOutput) == 0);
